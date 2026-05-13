807077971a6c1915017b57a9b97466c2fca79deb
lindyhanan <lindy24si@mahasiswa.pcr.ac.id>
2026-05-13 10:16:54 +0700

    pertemuan_9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,9 @@ android {
 }
 
 dependencies {
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.16.0")
+    implementation("androidx.gridlayout:gridlayout:1.1.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
