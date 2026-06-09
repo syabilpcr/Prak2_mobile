a1b0c78345314aecb7ac159cc3d1565ab8e97249
syabilpcr <syabil24si@mahasiswa.pcr.ac.id>
2026-06-09 21:33:45 +0700

    P11 'coba1'

--- app/src/main/java/com/example/bastardapps/Data/Api/NewsApiService.kt
+++ app/src/main/java/com/example/bastardapps/Data/Api/NewsApiService.kt
@@ -8,7 +8,7 @@ interface NewsApiService {
     @GET("top-headlines")
     suspend fun getTopHeadlines(
         @Query("country") country: String = "id",
-        @Query("apiKey") apiKey: String = "GANTI_DENGAN_API_KEY_KAMU"
+        @Query("apiKey") apiKey: String = "7c5b7c90d20c724079df29e8aa20f961"
         // Daftar gratis di: https://newsapi.org/register
     ): NewsResponse
 }
